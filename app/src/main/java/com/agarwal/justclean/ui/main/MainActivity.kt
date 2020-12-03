45eef772056e3ed1aa808cde7a2eb1e9e588cf3b
Sakshi Agarwal <devsakshiagarwal@gmail.com>
2020-12-03 17:00:47 +0400

    basic ui setup for the app

--- app/src/main/java/com/agarwal/justclean/ui/main/MainActivity.kt
+++ app/src/main/java/com/agarwal/justclean/ui/main/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.agarwal.justclean.ui.main
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.agarwal.justclean.R.id
 import com.agarwal.justclean.R.layout
 import com.agarwal.justclean.arch.network.Status.ERROR
 import com.agarwal.justclean.arch.network.Status.LOADING
 import com.agarwal.justclean.arch.network.Status.SUCCESS
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -31,17 +27,8 @@ class MainActivity : AppCompatActivity() {
     mainViewModel.getPosts()
     val sectionsPagerAdapter =
       SectionsPagerAdapter(this, supportFragmentManager)
-    val viewPager: ViewPager = findViewById(id.view_pager)
-    viewPager.adapter = sectionsPagerAdapter
-    val tabs: TabLayout = findViewById(id.tabs)
-    tabs.setupWithViewPager(viewPager)
-    val fab: FloatingActionButton = findViewById(id.fab)
-
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null)
-        .show()
-    }
+    view_pager.adapter = sectionsPagerAdapter
+    tabs_main.setupWithViewPager(view_pager)
   }
 
   private fun observeLiveData() {
